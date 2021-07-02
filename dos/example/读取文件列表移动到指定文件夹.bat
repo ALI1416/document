79d089463469a6cc16c679c498481b7881a1bc5b
ALI <1416978277@qq.com>
2021-07-02 16:33:43 +0800

    0702

--- dos/example/读取文件列表移动到指定文件夹.bat
+++ dos/example/读取文件列表移动到指定文件夹.bat
@@ -9,12 +9,13 @@ for /f "delims=: tokens=1,2" %%i in ('chcp') do (
 if not "%cp%"==" 65001" ( chcp 65001 & cls )
 
 echo 读取文件列表 移动到 指定文件夹
+echo.
 @REM 源文件夹
-set sfolder=Music
+set sfolder=Z:\music
 @REM 参考文件
-set file=aa.txt
+set file=Z:\music\_music_ck.txt
 @REM 目的文件夹
-set dfolder=aa
+set dfolder=D:\Music
 if not exist "%dfolder%" md "%dfolder%"
 for /f "tokens=1 delims=" %%i in (%file%) do (
     echo %%i
