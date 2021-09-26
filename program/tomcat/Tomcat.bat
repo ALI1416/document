6cf6aeff9c1fe43343a34c24ec1e0c57d99607e0
ALI <1416978277@qq.com>
2021-09-26 17:57:42 +0800

    0926

---
+++ program/tomcat/Tomcat.bat
@@ -0,0 +1,142 @@
+@echo off
+@REM Author:ALI
+@REM GitHub:https://github.com/ali1416
+@REM Version:1.0
+for /f "delims=: tokens=1,2" %%i in (' chcp ') do ( if not "%%j"==" 65001" ( chcp 65001 > nul ) )
+
+pushd %~dp0
+
+:begin
+
+echo.
+echo   ----------请选择操作----------
+
+echo   [1] 开启Tomcat服务
+
+echo   [2] 关闭Tomcat服务
+
+echo   [3] 开启Tomcat服务【隐藏窗口运行，必须安装Tomcat服务，需要以管理员身份运行】
+
+echo   --------------------
+
+echo   [4] 安装Tomcat服务【不会开机自启】
+
+echo   [5] 卸载Tomcat服务【不会开机自启】
+
+echo   [6] 开启Tomcat服务开机自启【需要以管理员身份运行】
+
+echo   [7] 关闭Tomcat服务开机自启【需要以管理员身份运行】
+
+echo   --------------------
+
+echo   [8] 用浏览器打开http://localhost:8080
+
+echo   --------------------
+
+echo   [Y] 创建Tomcat启动界面快捷方式到桌面
+
+echo   [Z] 获取管理员权限
+
+echo   [0] 退出
+
+echo   ----------请选择操作----------
+echo.
+
+choice /c 12345678YZ0
+if errorlevel 11 goto e0
+if errorlevel 10 goto ez
+if errorlevel 9 goto ey
+if errorlevel 8 goto e8
+if errorlevel 7 goto e7
+if errorlevel 6 goto e6
+if errorlevel 5 goto e5
+if errorlevel 4 goto e4
+if errorlevel 3 goto e3
+if errorlevel 2 goto e2
+if errorlevel 1 goto e1
+if errorlevel 0 goto e0
+
+:e1
+echo.
+echo   [1] 开启Tomcat服务
+echo.
+call bin\bin\startup.bat
+goto begin
+
+:e2
+echo.
+echo   [2] 关闭Tomcat服务
+echo.
+call bin\bin\shutdown.bat
+if %errorlevel%==0 ( echo 成功！ ) else ( echo 失败！ )
+goto begin
+
+:e3
+echo.
+echo   [3] 开启Tomcat服务【隐藏窗口运行，必须安装Tomcat服务，需要以管理员身份运行】
+echo.
+net start tomcat
+goto begin
+
+:e4
+echo.
+echo   [4] 安装Tomcat服务【不会开机自启】
+echo.
+call bin\bin\service.bat install tomcat
+goto begin
+
+:e5
+echo.
+echo   [5] 卸载Tomcat服务【不会开机自启】
+echo.
+call bin\bin\service.bat remove tomcat
+goto begin
+
+:e6
+echo.
+echo   [6] 开启Tomcat服务开机自启【需要以管理员身份运行】
+echo.
+sc config tomcat start=auto
+goto begin
+
+:e7
+echo.
+echo   [7] 关闭Tomcat服务开机自启【需要以管理员身份运行】
+echo.
+sc config tomcat start=disabled
+goto begin
+
+:e8
+echo.
+echo   [8] 用浏览器打开http://localhost:8080
+echo.
+start http://localhost:8080
+goto begin
+
+:ey
+echo.
+echo   [Y] 创建Tomcat启动界面快捷方式到桌面
+echo.
+call extra\createShortcut Desktop Tomcat "%~f0"
+goto begin
+
+:ez
+echo.
+echo   [Z] 获取管理员权限
+echo.
+( reg query "HKU\S-1-5-19">nul 2>&1 && goto ez1 )||( goto ez2 )
+:ez1
+echo 已经获取到了管理员权限，不必重复获取！
+goto begin
+:ez2
+( powershell Start-Process """%~f0""" -Verb RunAs )&&( exit )
+if %errorlevel%==0 goto end
+echo.
+echo 获取失败，请重试！
+goto begin
+
+:e0
+popd
+goto end
+
+:end
---
+++ program/tomcat/初始化.bat
@@ -0,0 +1,37 @@
+@echo off
+@REM Author:ALI
+@REM GitHub:https://github.com/ali1416
+@REM Version:1.0
+for /f "delims=: tokens=1,2" %%i in (' chcp ') do ( if not "%%j"==" 65001" ( chcp 65001 > nul ) )
+
+pushd %~dp0
+
+:begin
+
+echo 初始化前请仔细阅读README.md
+echo.
+choice /c yn /m "确定初始化吗？"
+if errorlevel 2 goto e2
+if errorlevel 1 goto e1
+if errorlevel 0 goto end
+
+:e1
+md bin
+pushd bin
+xcopy ..\..\bin bin /s /i /y
+xcopy ..\..\conf conf /s /i /y
+xcopy ..\..\lib lib /s /i /y
+xcopy ..\..\webapps webapps /s /i /y
+echo 初始化完成！
+popd
+popd
+pause
+goto end
+
+:e2
+echo 初始化取消！
+popd
+pause
+goto end
+
+:end
