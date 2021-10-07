7a88a8726fbfab4796b215f6f8e71ea6e5e0493d
ALI <1416978277@qq.com>
2021-10-07 11:21:02 +0800

    1007

--- dos/example/SMB端口445代理.bat
+++ dos/example/SMB端口445代理.bat
@@ -1,10 +1,13 @@
 @REM 代理222.186.139.72:5009到127.0.0.1:445
-netsh interface portproxy add v4tov4 listenport=445 listenaddress=127.0.0.1 connectport=5009 connectaddress=222.186.139.72
+netsh interface portproxy add v4tov4 listenaddress=127.0.0.1 listenport=445 connectaddress=222.186.139.72 connectport=5009
 
 @REM 删除代理127.0.0.1:445
 @REM netsh interface portproxy delete v4tov4 listenaddress=127.0.0.1 listenport=445
 
-@REM 关闭本机SMB服务
-@REM 控制面板->程序->程序和功能->启用或关闭Windows功能->关闭`SMB 1.0/CIFS 文件共享支持`和`SMB 直通`
-@REM 重启电脑
-@REM 访问\\127.0.0.1\
+@REM 显示全部代理
+@REM netsh interface portproxy show all
+
+@REM 关闭本机SMB服务：
+@REM 1. 控制面板->程序->程序和功能->启用或关闭Windows功能->关闭`SMB 1.0/CIFS 文件共享支持`和`SMB 直通`
+@REM 2. 重启电脑
+@REM 3. 访问\\127.0.0.1\
---
+++ program/dos2unix/README.md
@@ -0,0 +1,5 @@
+# LF/CR/CRLF互相转换
+
+当前版本号 7.4.2
+
+下载地址 https://waterlan.home.xs4all.nl/dos2unix.html
