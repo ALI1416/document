d8ef074b44462c19ae8b01f164ac0a213a8b9a8c
ALI <1416978277@qq.com>
2022-07-09 16:02:07 +0800

    0709

--- program/radar/_打开任务计划程序.bat
+++ program/radar/_打开任务计划程序.bat
@@ -2,7 +2,6 @@
 @REM Author:ALI
 @REM GitHub:https://github.com/ali1416
 @REM Version:1.0
-( reg query "HKU\S-1-5-19">nul 2>&1 )||( powershell Start-Process """%~f0""" -Verb RunAs )&&( exit )
 for /f "delims=: tokens=1,2" %%i in (' chcp ') do ( if not "%%j"==" 65001" ( chcp 65001 > nul ) )
 
-schtasks /create /sc onlogon /tn radar /tr """%~dp0radar.exe""" /rl highest
+start taskschd.msc
